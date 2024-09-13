855d12167c1ac4250a3cd3592f63387a0b1fc89b
Vahidyo <vahid.usefi@gmail.com>
2024-09-13 16:05:36 -0400

    final

--- routes/users.js
+++ routes/users.js
@@ -26,34 +26,67 @@ let users = [
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify({users}, null, 4))//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  // Find the user by email
+  const user = users.filter(u => u.email === email);
+
+  if (user) {
+    res.json(user); // Send the user data as the response
+  } else {
+    res.status(404).send("User not found\n"); // Return 404 if the user doesn't exist
+  }
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  users.push(req.query);  
+
+  res.send(users);//This line is to be replaced with actual return value
 });
 
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  email=req.params.email;
+  data=req.query
+
+  user = users.find(u => u.email === email)
+  
+  if(user){
+    data.firstName? user.firstName = data.firstName:null;
+    data.lastName? user.lastName = data.lastName:null;
+    data.DOB? user.DOB = data.DOB:null;
+    res.send(["user updated",user]);
+  }else{
+    res.send("user not found");
+  }
+
+  res.send()//This line is to be replaced with actual return value
 });
 
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  email = req.params.email;
+  index = users.findIndex(user => user.email === email);
+  
+  if(index !== -1){
+    const removedUser = users.splice(index,1);
+    res.send(`user with email ${email} removed\n`);
+  }else{
+    message = `user with email ${email} not found!\n`;
+    res.send(message)//This line is to be replaced with actual return value
+  }
 });
 
 module.exports=router;
